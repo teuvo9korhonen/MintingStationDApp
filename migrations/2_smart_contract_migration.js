38bfd76fc9b42d6cb1a76732aacb202e18474c00
teuvo9korhonen <81355902+teuvo9korhonen@users.noreply.github.com>
2021-12-19 22:20:40 +0300

    Delete migrations directory

--- migrations/2_smart_contract_migration.js
+++
@@ -1,5 +0,0 @@
-const SmartContract = artifacts.require("TheBeeCollaborative"); // Change this to what your Contract Name is in Solidity
-
-module.exports = function (deployer) {
-  deployer.deploy(SmartContract, "Name", "Symbol", "https://"); //
-};
